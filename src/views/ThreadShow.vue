85d4ae062d0d1e3b214f070fb722603bb924bb83
Bryan Yunis <bbyunis@gmail.com>
2018-11-25 07:51:07 -0700

    cleanup components; move create postlistitem component

--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -1,37 +1,13 @@
 <template>
   <div>
       <h1>{{thread.title}}</h1>
-
-      <div class="post-list">
-        <div v-for="postId in thread.posts"
-             class="post"
-        >
-          <div class="user-info">
-            <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
-
-            <a href="#">
-              <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="">
-            </a>
-
-            <p class="desktop-only text-small">107 posts</p>
-          </div>
-
-          <div class="post-content">
-            <div>
-              {{posts[postId].text}}
-            </div>
-          </div>
-
-          <div class="post-date text-faded">
-            {{posts[postId].publishedAt}}
-          </div>
+      <PostList :posts='posts'/>
         </div>
-      </div>
-    </div>
 </template>
 
 <script>
 import sourceData from "@/data/data.json";
+import PostList from "@/components/PostList";
 export default {
   props: {
     id: {
@@ -39,13 +15,21 @@ export default {
       type: String
     }
   },
+  components: {
+    PostList
+  },
   data() {
     return {
-      threads: sourceData.threads,
-      thread: sourceData.threads[this.id],
-      posts: sourceData.posts,
-      users: sourceData.users
+      thread: sourceData.threads[this.id]
     };
+  },
+  computed: {
+    posts() {
+      const postIds = Object.values(this.thread.posts);
+      return Object.values(sourceData.posts).filter(post =>
+        postIds.includes(post[".key"])
+      );
+    }
   }
 };
 </script>
